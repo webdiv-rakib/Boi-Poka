3246fc7cc4a3db37e76f87814cab667859b2872b
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-13 21:59:28 +0600

    banner section design complete.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import bookimage from '../../assets/books.jpg'
+import bookimage from '../../assets/books-removebg-preview.png'
 const Banner = () => {
     return (
-        <div className='md:flex md:justify-center md:gap-10'>
-            <div>
-                <h1>Books to freshen up your</h1>
+        <div className='md:flex md:justify-center md:mt-10'>
+            <div className='mt-30 space-y-6'>
+                <h1 className='text-7xl'>Books to freshen up<br/>your bookshelf</h1>
                 <button className='btn'>View The List</button>
             </div>
             <div>
-                <img src={bookimage} alt="" />
+                <img className='rounded-2xl' src={bookimage} alt="" />
             </div>
         </div>
     );
