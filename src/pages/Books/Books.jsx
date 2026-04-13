1131c40f7fed2481edbce01a76691617c0fe22b4
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-13 22:07:38 +0600

    fetch data by using useState and useEffect.

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
+    const [allBooks, setAllBooks] = useState([]);
+    useEffect(() => {
+        fetch("booksData.json")
+            .then(res => res.json())
+            .then(data => setAllBooks(data))
+    }, [])
     return (
         <div>
             <h1 className='text-center text-4xl'>Books</h1>
