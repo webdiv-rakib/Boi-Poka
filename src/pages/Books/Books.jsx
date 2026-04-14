f82c1b38742e8b7868de2050040f64b4b4108cd3
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-04-14 08:28:37 +0600

    card section redesign and responsive.

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -6,7 +6,7 @@ const Books = ({ data }) => {
         <div className='bg-base-200'>
             <h1 className='text-center text-4xl font-bold'>Books</h1>
             <Suspense fallback={<span>loading.....</span>}>
-                <div className='grid grid-cols-4 justify-self-center gap-5 mt-10'>
+                <div className='md:grid md:grid-cols-4 justify-self-center md:gap-5 mt-10 grid grid-cols-2 gap-5 p-5'>
                     {
                         data.map(singleBook => <Book
                             key={singleBook.bookId}
